ae042c9a2eb7a1be26d6c0008073acb95fd813fc
Gabriella MoldvaiSajti <gabi.sajti@gmail.com>
2019-06-18 09:33:04 +0200

    modify

--- gyak_0617/gyak_06174.js
+++ gyak_0617/gyak_06174.js
@@ -3,23 +3,24 @@ const cezar = (str, n) => {
   let newstring = '';
   let numbers = [];
   let newnumbers = [];
+  let newnumber = n % 26;
   for (let i = 0; i < arrstr.length; i++) {
     numbers[i] = str.charCodeAt(i);
   }
   console.log(numbers);
   for (let j = 0; j < numbers.length; j++) {
     if (numbers[j] >= 65 && numbers[j] <= 90) {
-      if (numbers[j] + n <= 90) {
-        newnumbers[j] = numbers[j] + n;
+      if (numbers[j] + newnumber <= 90) {
+        newnumbers[j] = numbers[j] + newnumber;
       } else {
-        newnumbers[j] = 64 + (numbers[j] + n - 90);
+        newnumbers[j] = 64 + (numbers[j] + newnumber - 90);
       }
     }
     if (numbers[j] >= 97 && numbers[j] <= 122) {
-      if (numbers[j] + n <= 122) {
-        newnumbers[j] = numbers[j] + n;
+      if (numbers[j] + newnumber <= 122) {
+        newnumbers[j] = numbers[j] + newnumber;
       } else {
-        newnumbers[j] = 96 + (numbers[j] + n - 122);
+        newnumbers[j] = 96 + (numbers[j] + newnumber - 122);
       }
     }
   }
